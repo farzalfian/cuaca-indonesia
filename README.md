167f79022d4f379e721bf68af49aefec0b8f94f4
farza <farzafian4@gmail.com>
2024-05-19 22:01:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web cuaca yang simple 
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f573ef7acdmsh78a569fd8582d1ep189051jsnf6429f61f73a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "fc458c7184f744ec11f521ced20ca0c0"
